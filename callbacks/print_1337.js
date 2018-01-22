6ab8fded1bf68233d2dd1f0a8c1fb310f679f718
Taylor <thulsmans.2994@me.com>
2018-01-22 21:00:26 +0000

    leetspeak

--- callbacks/print_1337.js
+++ callbacks/print_1337.js
@@ -1,2 +1,29 @@
 const https = require('https');
 const step4 = require('../step-4/step-4.js');
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step6/lowercase.html'
+};
+
+function leetspeak(html) {
+  output = html
+  let cipher = {
+    '4': /a/g,
+    '3': /e(?!r)/g,
+    '1': /l/g,
+    '0': /o(?!u)/g,
+    '5': /s/g,
+    '7': /t/g,
+    'x': /ck/g,
+    '0r': /er/g,
+    'j00': /you/g
+
+  };
+  for (key in cipher) {
+    output = output.replace(cipher[key], key);
+  }
+  console.log(output);
+}
+
+
+step4.getHTML(requestOptions, leetspeak)
